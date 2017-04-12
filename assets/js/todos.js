d6c6e70966a84e1fd75ccdcd11af23344c7b9c9b
Anna Markowicz <anna.markowicz5@gmail.com>
2017-04-12 17:55:40 +0200

    Add new items to ToDo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,5 @@
 //Check off specific Todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li",  function(){
 	$(this).toggleClass("completed");
 });
 
@@ -9,4 +9,14 @@ $("span").click(function(event){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function(event){
+	if(event.which === 13){
+	//grabbing new todo text from input
+	var todoText = $(this).val();
+	$(this).val("")
+	//create  new li and add to ul
+	$("ul").append("<li><span>X </span>" + todoText + "</li>");
+	}
 });
